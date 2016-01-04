a392f014dfd7ebdcccc3b07bc2959ba0f988036a
VictorDiCampos <victordicampos@gmail.com>
2016-01-04 19:07:54 -0200

    Adding: Slowly scroll of navbar using jquery.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function scroll(){
-  document.getElementById('titulo3').scrollIntoView(2000);
-  // window.setTimeout(2000);
-}
-//scroll();
+// var scroll = document.getElementsByTagName('a');
+// function pageScroll() {
+//   window.scrollBy(0,50);
+//   scrolldelay = setTimeout('pageScroll()',100); // scrolls every x milliseconds
+// }
+// scroll.addEventListener('click', pageScroll);
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
